5ace8f070a26af40e8abbea4db4623247fa16916
wonchanLee <dldnjscks19@naver.com>
2021-06-13 03:03:08 -0400

    detaile update

--- views/indexpage.js
+++ views/indexpage.js
@@ -67,18 +67,17 @@ module.exports = {
   <header class="blog-header py-3">
     <div class="row flex-nowrap justify-content-between align-items-center">
       <div class="col-4 pt-1">
-        <a class="link-secondary" href="#">Subscribe</a>
+        ${nickname}${ment}
       </div>
       <div class="col-4 text-center">
         <a class="blog-header-logo text-dark" href="#">Wonchan</a>
       </div>
       <div class="col-4 d-flex justify-content-end align-items-center">
-        <!--added-->
-        ${nickname}${ment} &nbsp; &nbsp;
-        ${loginhref}
-        <a class="link-secondary" href="#" aria-label="Search">
+        <!--added--> 
+        ${loginhref}&nbsp;
+       <!-- <a class="link-secondary" href="#" aria-label="Search">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3" role="img" viewBox="0 0 24 24"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"/><path d="M21 21l-5.2-5.2"/></svg>
-        </a>
+        </a> -->
         <a class="btn btn-sm btn-outline-secondary" href="/login/signin">Sign in</a>
       </div>
     </div>
@@ -167,8 +166,8 @@ module.exports = {
       </h3>
 
       <article class="blog-post">
-        <h2 class="blog-post-title">자기 소개</h2>
-        <p class="blog-post-meta">January 1, 2021 by <a href="#">Mark</a></p>
+        <h2 class="blog-post-title">이원찬</h2>
+        <p class="blog-post-meta">April 3th, 2000</p>
 
         <p>안녕</p>
         <hr>
--- views/read/readpage.js
+++ views/read/readpage.js
@@ -59,7 +59,7 @@ module.exports = {
        if(comment[i].author == ''){ comment[i].author = 'guest';}
        com+=`<div class="card shadow-sm">
             <svg class="bd-placeholder-img card-img-top" width="100%" height="50" xmlns="http://www.w3.org/2000/svg" role="img"  preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="black"/><text x="17" y="50%" fill="#eceeef" dy=".3em">
-              ${comment[i].author}</text></svg>
+              작성자: ${comment[i].author}</text></svg>
             <div class="card-body">
               <p class="card-text">${comment[i].comment}</p>
                 <div class="d-flex justify-content-between align-items-center">
